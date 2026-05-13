78f4c2543138570caafcb52e6100226e32fb3626
ambujt-aws <53196504+ambujt-aws@users.noreply.github.com>
2026-05-13 10:53:06 -0700

    fix: Remove unused IAM role IamRoleStack-LambdaAdminRole635E17BF-SEwbXzsGnyAp
    AWS IAM Access Analyzer identified this role as unused.
    Finding ID: 83294a63-d33b-4d02-8d3a-0064022c523f

--- lib/iam-role-stack.ts
+++ lib/iam-role-stack.ts
@@ -4,10 +4,5 @@ import { Construct } from 'constructs';
 export class IamRoleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // IAM Role 'LambdaAdminRole' was removed because it was identified as unused
-    // by AWS IAM Access Analyzer.
-    // Finding ID: 83294a63-d33b-4d02-8d3a-0064022c523f
-    // Original Resource ARN: arn:aws:iam::058264309950:role/IamRoleStack-LambdaAdminRole635E17BF-SEwbXzsGnyAp
   }
 }
